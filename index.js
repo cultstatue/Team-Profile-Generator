9e41cf00737f962ffc27fe5389de87e235904387
cultstatue <saltedchurros@gmail.com>
2022-07-27 22:29:29 -0400

    added all user questions and passed them through classes

--- index.js
+++ index.js
@@ -1,22 +1,56 @@
 const inquirer = require('inquirer');
-const Engineer = require('./lib/Engineer');
+const generateFile = require('./src/page-template')
+const Manager = require('./lib/Manager')
 const Intern = require('./lib/Intern')
-const Manager = require('./lib/Manager');
-// const { writeFile, copyFile } = require('./utils/generate-site.js');
+const Engineer = require('./lib/Engineer')
 
+function employeeData() {
 
-const promptManager = () => {
+    this.manager;
+    this.interns = [];
+    this.engineers = [];
 
-    return inquirer.prompt([
+}
+
+employeeData.prototype.writeFile = function() {
+    
+    generateFile(this);
+}
+employeeData.prototype.chooseNext = function() {
+
+    inquirer
+    .prompt({
+
+        type: 'list',
+        name: 'more',
+        message: 'Would you like to enter another employee or end the application?',
+        choices: ['Add an Engineer', 'Add an Intern', 'Finalize team']
+
+    })
+     .then(choice => {
+        if (choice.more === 'Finalize team') {
+            this.writeFile();
+        } else if (choice.more === 'Add an Intern') {
+            this.addIntern();
+        } else if (choice.more === 'Add an Engineer') {
+            this.addEngineer();
+        }     
+    })
+}
+
+employeeData.prototype.addEngineer = function() {
+
+    inquirer
+        .prompt([
         {
             type: 'input',
             name: 'name',
-            message: 'What is your team manager name?',
+            message: 'What is your engineers name?',
             validate: nameInput => {
                 if (nameInput) {
                     return true;
                 } else {
-                    console.log('Please enter your team manager name.');
+                    console.log('Please enter your engineers name.');
                     return false;
                 }
             }
@@ -25,12 +59,12 @@ const promptManager = () => {
         {
             type: 'input',
             name: 'email',
-            message: 'What is your team manager email address?',
+            message: 'What is your engineers email address?',
             validate: emailInput => {
                 if (emailInput) {
                     return true;
                 } else {
-                    console.log('Please enter your team manager email.');
+                    console.log('Please enter your engineers email.');
                     return false;
                 }
             }
@@ -38,80 +72,169 @@ const promptManager = () => {
         {
             type: 'input',
             name: 'id',
-            message: 'What is your team manager employee ID?',
+            message: 'What is your engineers employee ID?',
             validate: idInput => {
                 if (idInput) {
                     return true;
                 } else {
-                    console.log('Please enter your team manager ID.');
+                    console.log('Please enter your engineers ID.');
                     return false;
                 }
             }
         },
         {
             type: 'input',
-            name: 'office',
-            message: 'What is your team manager office number?',
-            validate: officeInput => {
-                if (officeInput) {
+            name: 'github',
+            message: 'What is your engineers github username?',
+            validate: schoolInput => {
+                if (schoolInput) {
                     return true;
                 } else {
-                    console.log('Please enter your team manager office number.');
+                    console.log('Please enter your engineers github.');
+                    return false;
+                }
+            }
+        }
+    ])
+    .then((engineerData) => {   
+
+        this.engineers.push(new Engineer(engineerData.name, engineerData.id, engineerData.email, engineerData.github));
+
+        this.chooseNext();
+    })
+}
+
+employeeData.prototype.addIntern = function() {
+
+    inquirer
+        .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your interns name?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your interns name.');
                     return false;
                 }
             }
         },
-    ])
-}
 
-const promptMore = () => {
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your interns email address?',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your interns email.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your interns employee ID?',
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your interns ID.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What is your interns school?',
+            validate: schoolInput => {
+                if (schoolInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your interns school.');
+                    return false;
+                }
+            }
+        }
+    ])
+    .then((internData) => {   
+
+        this.interns.push(new Intern(internData.name, internData.id, internData.email, internData.school));
+
+        this.chooseNext();
+    })
+}
+employeeData.prototype.initializeApp = function() {                 
 
     inquirer
-    .prompt
-    ({
-        type: 'list',
-        name: 'more',
-        message: 'Would you like to enter another employee or end the application?',
-        choices: ['Add an Engineer', 'Add an Intern', 'Quit and generate page']
+    .prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your team managers name?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your team managers name.');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your team managers email address?',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Please enter your team managers email.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is your team managers employee ID?',
+        validate: idInput => {
+            if (idInput) {
+                return true;
+            } else {
+                console.log('Please enter your team managers ID.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'office',
+        message: 'What is your team managers office number?',
+        validate: officeInput => {
+            if (officeInput) {
+                return true;
+            } else {
+                console.log('Please enter your team managers office number.');
+                return false;
+            }
+        }
+    }
+    ])
+    .then((managerData) => {   
+
+        this.manager = new Manager(managerData.name, managerData.id, managerData.email, managerData.office)
+
+        this.chooseNext();
     })
-    .then(choice => {
-        if (choice.more === 'Quit and generate page') {
-            console.log(choice);
-        } else if (choice.more === 'Add an Intern') {
-            console.log('intern');
-        } else if (choice.more === 'Add an Engineer') {
-            console.log('engineer');
-        } 
-    })
+
 }
 
-promptManager()
-.then(promptMore)
 
-
-
-
-
-// .then(managerData => {
-//     if (managerData.list === 'Quit and generate page') {
-//         console.log("quit");
-//     } else if (managerData.list === 'Add an Intern') {
-//         console.log("add an intern");
-//     } else if (managerData.list === 'Add an Engineer') {
-//         console.log("Add an Engineer");
-//     }
-
-// }) 
-
-
-
-
-
-// .then(({ name, email, id, office }) => {        
-//     const manager = new Manager(name, email, id, office);
-// })
-
-// if (!employeeData.employees) {
-
-//     employeeData.employees = []
-// }
+new employeeData().initializeApp();
---
+++ src/page-template.js
@@ -0,0 +1,10 @@
+// const Manager = require('../lib/Manager')
+module.exports = templateData => {
+
+    const { interns, engineers, manager } = templateData
+
+    console.log(manager)
+    console.log(interns)
+    console.log(engineers)
+    
+}
